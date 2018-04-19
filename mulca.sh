d9ba820f07bd3946e11a0e9aed6a5dd88cc21970
Minpu Kang <kangminpu@126.com>
2018-04-19 11:32:34 +0800

    fix if certificate file modified in windows
    fix to read if certificate file modified in windows

--- mulca.sh
+++ mulca.sh
@@ -2,7 +2,10 @@
 
 ################################################################
 #  Multi Certificates in one file checker                      #
-#  Version:1.0                                                 #
+#  Version:2.0                                                 #
+#          1.0: the first version                              #
+#          2.0: fix to read if certificate file modified in    #
+#               in windows.                                    #
 #  Owner: Minpu Kang                                           #
 #  Introduction: This is used to check multiple certificates   #
 #                in one file based on openssl.                 #
@@ -21,22 +24,30 @@
 #                                                              #
 ################################################################
 
-sub_ca_file="certificatexx.crt"
-ca_number=1
-if [ -f  $sub_ca_file ];then
-    rm -rf $sub_ca_file
+sub_ca_file="/tmp/sub_ca_tmpxxxxx.crt"  #temporary ca file for each certificate fetched from the input
+ca_number=1     #used to counte the certificate number
+if [ -f  $sub_ca_file ];then            #check if the temporary ca file existed or not,
+    rm -rf $sub_ca_file                 #exist, then remove
 fi
 
-if [ -z $1 ];then
+if [ -z $1 ];then        #the input file is required
     echo "ERROR: missing the file name";exit
 fi
 
+#if the input file existed or not, yes, continue; no, exit.
 if [ -f $1 ];then
-    ca_total_number=`cat $1 | grep -w "\-----BEGIN\ CERTIFICATE-----" -c`
-    if [ $ca_total_number != "0" ];then 
-        echo "Total Certificate found: $ca_total_number";echo
-        while read line || [ -n "$line" ]
+    ca_total_number=`cat $1 | grep -w "\-----BEGIN\ CERTIFICATE-----" -c`   #check if the input file has certificate or not, and how many certificates are there
+    if [ $ca_total_number != "0" ];then   #if there is certificate, going on
+        echo "Total Certificate found: $ca_total_number";echo         #printout the totle certificates number
+        
+        #this is used for the ca file modifyed in windows
+        tmp_ca_file="/tmp/"$1"tmp.crt"     #temporary file of the input
+        sed 's/\r//g' $1 > $tmp_ca_file    #replace the line break of windows and write into the temporary file, the input file is not changed.
+        
+        while read line || [ -n "$line" ]  #read the certificate in the temporary file 
         do
+            #if it is end of certificate, then perform the openssl to printout the x509 format
+            #if not, continue to write the certificate to temporary ca file
             if [[ $line == "-----END CERTIFICATE-----" ]];then
                 echo $line >> $sub_ca_file 2>&1
                 echo "########Certificate $ca_number#######"
@@ -46,10 +57,11 @@ if [ -f $1 ];then
             else
                 echo $line >> $sub_ca_file 2>&1
             fi 
-        done < $1 
+        done < $tmp_ca_file
+        rm -rf $tmp_ca_file 2>&1
     else 
         echo;echo "An incorrecte certificate file.";echo;echo
     fi 
 else
     echo "File \"$1\" is not existed!"
-fi 
+fi
